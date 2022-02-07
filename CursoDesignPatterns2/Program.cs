7b27eb0d43f170c6f0f2477922cd804e62a7894d
MarcelloAffonso <mars1803@gmail.com>
2022-02-06 21:10:58 -0300

    Add project files.

---
+++ CursoDesignPatterns2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CursoDesignPatterns2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDbConnection conexao = new ConnectionFactory().GetConnection();
+
+            IDbCommand comando = conexao.CreateCommand();
+            comando.CommandText = "select * from tabela";
+        }
+    }
+}
